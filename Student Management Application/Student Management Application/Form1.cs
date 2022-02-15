c444e647758fa75126e6cc5162da6634901508ba
suthada <96216472+suthada@users.noreply.github.com>
2022-02-15 23:03:22 +0700

    CSV

--- Student Management Application/Student Management Application/Form1.cs
+++ Student Management Application/Student Management Application/Form1.cs
@@ -11,30 +11,35 @@ namespace Student_Management_Application
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "CSV (*.csv) | *.csv";
             if(openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 string[] readAllLine = File.ReadAllLines(openFileDialog.FileName);
                 string readAllText = File.ReadAllText(openFileDialog.FileName);
-                this.textBox1.Text = readAllText;
+                //this.textBox1.Text = readAllText;
+                //this.dataGridView1.Rows.Add()
+                addDataToGridview("01","name","cis");
             }
+            
+        }
+        private void addDataToGridview(string id, string name, string major)
+        {
+            this.dataGridView1.Rows.Add(new string[] { "id", "name", "major" });
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        { 
 
-            //string data = this.dataGridView1.Rows[0].Cells[0].Value.ToString();
-
-
-            string filepath = string.Empty;
+            string strData = string.Empty;
+            
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "CSV (*.csv) | *.csv";
             if(saveFileDialog.ShowDialog() == DialogResult.OK)
             {
                 if(saveFileDialog.FileName != string.Empty)
                 {
-                    filepath = saveFileDialog.FileName;
 
                     int row = this.dataGridView1.Rows.Count;
                     for(int i = 0; i < row; i++)
@@ -42,12 +47,13 @@ namespace Student_Management_Application
                         int column = this.dataGridView1.Columns.Count;
                         for(int j = 0; j < column; j++)
                         {
-                            this.dataGridView1.Rows[i].Cells[j].Value.ToString();
+                            strData = this.dataGridView1.Rows[i].Cells[j].Value.ToString();
+                            //TODO make data from dataGridViewl to variable
                         }
                     }
 
                     //save file
-                    File.WriteAllText(saveFileDialog.FileName, this.textBox1.Text, Encoding.UTF8);
+                    File.WriteAllText(saveFileDialog.FileName, strData, Encoding.UTF8);
                 }
             }
 
---
+++ Student Management Application/Student Management Application/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Management_Application
+{
+    internal class Student
+    {
+        private string _id;
+        private string _name;
+        private string _major;
+
+        //encapsulation
+        public string getId() { return _id; }
+        public string getName() { return _name; }
+        public string getMajor() { return _major;}
+    }
+}
